ceb72a71c1959c326963f5626ea52816bfe30929
m.sabatowski <ma.sabatowski@gmail.com>
2022-05-06 15:23:40 +0200

    second commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,18 @@
 import java.io.File
 
 object Main extends App {
-  def getListOfSubDirectories(directoryName: String): Array[String] = {
-    (new File(directoryName))
-      .listFiles
-      .map(_.getName)
+  if (args.length < 1) {
+    println("ERROR: brak argumentu")
+    sys.exit(-1)
   }
-  getListOfSubDirectories(args(0)).foreach(println)
+  val path = args(0)
+  val isDirAvailable = new File(path).exists()
+  if (!isDirAvailable) {
+    println(s"ERROR: nie ma takiej ścieżki: $path")
+    sys.exit(-1)
+  }
+  new File(path)
+    .listFiles
+    .map(_.getName)
+    .foreach(println)
 }
